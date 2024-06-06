87c9caf4c8e7490858a0c4e6b77e21ef49d1f41f
Huallison Tavares <huallison.tavares@aluno.ce.gov.br>
2024-06-06 12:52:59 -0300

    Continuando projeto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\BibliotecaController;
-use App\Http\Controllers\BibliotecaController;
 use App\Http\Controllers\UserLoginController;
 use App\Http\Controllers\UserRegisterController;
 use Illuminate\Support\Facades\Route;
@@ -25,7 +24,7 @@ Route::post('login', [UserLoginController::class, 'login'])->name("login");
 Route::middleware("auth")->prefix("dashboard")->group(function () {
     Route::get("/", [BibliotecaController::class, 'index'])->name('dashboard');
     Route::get("logout", [UserLoginController::class, 'logout'])->name('logout');
-    Route::get("cadastrar", [BibliotecaController::class, 'index'])->name('book-register');
+    Route::get("cadastrar", [BibliotecaController::class, 'registerBook'])->name('book-register');
     Route::post("cadastrar", [BibliotecaController::class, 'store'])->name('book-register');
 });
 
